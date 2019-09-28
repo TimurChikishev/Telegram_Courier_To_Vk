dc7485ff4a68ba856904e6c2ec08638074b65a7b
Тимур <43215185+TimurChikishev@users.noreply.github.com>
2019-09-28 15:13:08 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
-token_telegram = '988352904:AAHMeSsk5_oH1srEh3lLb_9bcctfh90N6m4'
-token_vk = 'fc6352bdfd0bb77a7a9af45c6e7e560427bfc82285e495f6fa9b41a0cc8ab077a9e1e5693f019eaa6e871'
+token_telegram = ''
+token_vk = ''
 
 correct_id = {
-    '968308276' : 'Даньшиной',
-    '943954959' : 'Гневшева'
+    'telegram_id' : 'mis',
+    'telegram_id' : 'mr'
 }
 
 groups = {
